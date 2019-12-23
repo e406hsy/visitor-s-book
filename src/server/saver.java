7d43c7901f85b49742843f741ca9d7f4d4959bdd
e406hsy <e406hsy@gmail.com>
2019-12-24 01:50:36 +0900

    new file:   WebContent/WEB-INF/web.xml 	new file:   build/classes/server/CharacterEncodingFilter.class 	new file:   build/classes/server/bookList.class 	modified:   build/classes/server/saver.class 	new file:   src/server/CharacterEncodingFilter.java 	new file:   src/server/bookList.java 	modified:   src/server/saver.java

--- src/server/saver.java
+++ src/server/saver.java
@@ -21,72 +21,80 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/saver")
 public class saver extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public saver() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-    
-    
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public saver() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
 		request.setCharacterEncoding("UTF-8");
 		String email = request.getParameter("email");
 		String password = request.getParameter("password");
 		String content = request.getParameter("content");
-		
+
 		// 클라이언트로 출력하기 위해 준비한다.
 		response.setContentType("text/html; charset=UTF-8");
 		PrintWriter out = response.getWriter();
-		
+
 		out.println("<html><body>");
-		out.println("<h1>로그인 결과</h1>");
-		out.println("email : "+email);
-		out.println("password : "+password);
-		out.println("content : "+content);
-		out.println("</body></html>");
+		out.println("<h1>방명록 작성 결과</h1>");
+		out.println("email : " + email + "<br>");
+		out.println("password : " + password + "<br>");
+		out.println("content : " + content + "<br>");
 
 		Connection conn = null;
 		PreparedStatement stmt = null;
 
 		try {
-			DriverManager.registerDriver(new com.mysql.jdbc.Driver());
-			conn = DriverManager.getConnection(
-					"jdbc:mysql://127.0.0.1:3306/visitor-s-book?characterEncoding=UTF-8&serverTimezone=UTC", //JDBC URL
-					"admin",	// DBMS 사용자 아이디
-					"admin");	// DBMS 사용자 암호
 			ServletContext sc = this.getServletContext();
-			stmt = conn.prepareStatement(
-					"INSERT INTO BOOK(EMAIL,PASSWORD,CONTENT,GEN_TIME)"
-					+ " VALUES (?,?,?,NOW())");
+			Class.forName(sc.getInitParameter("driver"));
+			conn = DriverManager.getConnection(
+					sc.getInitParameter("url"), // JDBC URL
+					sc.getInitParameter("username"), // DBMS 사용자 아이디
+					sc.getInitParameter("password")); // DBMS 사용자 암호
+			stmt = conn.prepareStatement("INSERT INTO BOOK(EMAIL,PASSWORD,CONTENT,GEN_TIME)" + " VALUES (?,?,?,NOW())");
 			stmt.setString(1, request.getParameter("email"));
 			stmt.setString(2, request.getParameter("password"));
 			stmt.setString(3, request.getParameter("content"));
 			stmt.executeUpdate();
-			
+			out.println("성공적으로 등록되었습니다.<br>");
+
 		} catch (Exception e) {
 			throw new ServletException(e);
-			
+
 		} finally {
-			try {if (stmt != null) stmt.close();} catch(Exception e) {}
-			try {if (conn != null) conn.close();} catch(Exception e) {}
+			try {
+				if (stmt != null)
+					stmt.close();
+			} catch (Exception e) {
+			}
+			try {
+				if (conn != null)
+					conn.close();
+			} catch (Exception e) {
+			}
 		}
+		out.println("</body></html>");
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		doGet(request, response);
+		response.sendRedirect("index");
 	}
 
 }
