ce62665d7d257f17d64ff44f2c623a860f832d58
e406hsy <e406hsy@gmail.com>
2019-12-25 01:35:07 +0900

    modified:   build/classes/server/BookListServlet.class 	modified:   src/server/BookListServlet.java

--- src/server/BookListServlet.java
+++ src/server/BookListServlet.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.Statement;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 
 import javax.servlet.RequestDispatcher;
